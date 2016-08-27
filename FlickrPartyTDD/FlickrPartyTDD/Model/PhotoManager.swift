8d8e2dde8a863adbe37192c272680ad3953b115e
Chetan Agarwal <apps@appscool.in>
2016-08-27 15:10:56 +0530

    Moved model classes to Model folder Added PhotoManager

---
+++ FlickrPartyTDD/FlickrPartyTDD/Model/PhotoManager.swift
@@ -0,0 +1,13 @@
+//
+//  PhotoManager.swift
+//  FlickrPartyTDD
+//
+//  Created by Chetan Agarwal on 27/08/16.
+//  Copyright © 2016 DeuxLapins. All rights reserved.
+//
+
+import Foundation
+
+class PhotoManager {
+    
+}
---
+++ FlickrPartyTDD/FlickrPartyTDDTests/Model/PhotoManagerTests.swift
@@ -0,0 +1,18 @@
+//
+//  PhotoManagerTests.swift
+//  FlickrPartyTDD
+//
+//  Created by Chetan Agarwal on 27/08/16.
+//  Copyright © 2016 DeuxLapins. All rights reserved.
+//
+
+import XCTest
+@testable import FlickrPartyTDD
+
+class PhotoManagerTests: XCTestCase {
+    
+    func testPhotoManager_ReturnsCountOfPhotos() {
+        _ = PhotoManager()
+    }
+    
+}
