fbd774dba245d6548a4a6526adc2337deb2b9994
Chetan Agarwal <apps@appscool.in>
2016-08-27 11:50:23 +0530

    Blank iOS App Target, Unit Test Target & UI Test Target

---
+++ FlickrPartyTDD/FlickrPartyTDD/AppDelegate.swift
@@ -0,0 +1,21 @@
+//
+//  AppDelegate.swift
+//  FlickrPartyTDD
+//
+//  Created by Chetan Agarwal on 27/08/16.
+//  Copyright © 2016 DeuxLapins. All rights reserved.
+//
+
+import UIKit
+
+@UIApplicationMain
+class AppDelegate: UIResponder, UIApplicationDelegate {
+
+    var window: UIWindow?
+
+    
+    func application(application: UIApplication, didFinishLaunchingWithOptions launchOptions: [NSObject: AnyObject]?) -> Bool {
+        return true
+    }
+}
+
