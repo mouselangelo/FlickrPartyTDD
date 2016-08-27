3440781af6512153dd3420ea00836453cae953a3
Chetan Agarwal <apps@appscool.in>
2016-08-27 16:34:59 +0530

    Added basic functions to PhotoManager

--- FlickrPartyTDD/FlickrPartyTDD/Model/PhotoManager.swift
+++ FlickrPartyTDD/FlickrPartyTDD/Model/PhotoManager.swift
@@ -9,5 +9,26 @@
 import Foundation
 
 class PhotoManager {
+    var count: Int = 0
+    
+    private var photos = [Photo]()
+    
+    func addPhoto(photo: Photo) {
+        count += 1
+        photos.append(photo)
+    }
+    
+    func photoAtIndex(index: Int) -> Photo? {
+        guard (index >= 0 && index < photos.count) else {
+            return nil
+        }
+        
+        return photos[index]
+    }
+    
+    func removeAll() {
+        count = 0
+        photos.removeAll()
+    }
     
 }
--- FlickrPartyTDD/FlickrPartyTDDTests/Model/PhotoManagerTests.swift
+++ FlickrPartyTDD/FlickrPartyTDDTests/Model/PhotoManagerTests.swift
@@ -11,8 +11,63 @@ import XCTest
 
 class PhotoManagerTests: XCTestCase {
     
-    func testPhotoManager_ReturnsCountOfPhotos() {
-        _ = PhotoManager()
+    var sut: PhotoManager!
+
+    lazy var testPhoto: Photo = {
+        let testURL = NSURL(string: "http://example.com")!
+       return PhotoItem(url: testURL, thumbnailURL: testURL, title: "Test")
+    }()
+    
+    override func setUp() {
+        sut = PhotoManager()
     }
     
+    func testPhotoManager_ReturnsCountOfPhotos() {
+        XCTAssertEqual(sut.count, 0, "Initial count must be 0")
+    }
+    
+    func testAddingPhotos_IncreasesCount() {
+        sut.addPhoto(testPhoto)
+        XCTAssertEqual(sut.count, 1, "Count should be same as number of photos added")
+    }
+    
+    func testGetPhotoAtIndex_ShouldReturnTheCorrectPhoto() {
+        sut.addPhoto(testPhoto)
+        let firstPhoto = sut.photoAtIndex(0)
+        
+        XCTAssertEqual(firstPhoto?.url, testPhoto.url,
+                       "Should be the photo we just added")
+    }
+    
+    func testGetPhotoAtIndex_ForDifferentIndices_ShouldReturnTheCorrectPhoto() {
+        sut.addPhoto(testPhoto)
+        
+        let anotherPhoto = PhotoItem(url: NSURL(string: "http://example2.com")!, thumbnailURL: NSURL(string: "http://example2.com")!, title: "Another photo")
+        
+        sut.addPhoto(anotherPhoto)
+        
+        let firstPhoto = sut.photoAtIndex(0)
+        let secondPhoto = sut.photoAtIndex(1)
+        
+        XCTAssertEqual(firstPhoto?.url, testPhoto.url,
+                       "Should be the first photo")
+        
+        XCTAssertEqual(secondPhoto?.url, anotherPhoto.url,
+                       "Should be the second photo")
+    }
+    
+    func testRemoveAll_SetsCountToZero() {
+        sut.addPhoto(testPhoto)
+        XCTAssertEqual(sut.count, 1, "Count should be same as number of photos added")
+        sut.removeAll()
+        XCTAssertEqual(sut.count, 0, "Count should be 0 after remove all")
+    }
+    
+    func testRemoveAll_ShouldRemoveAllItems() {
+        sut.addPhoto(testPhoto)
+        XCTAssertEqual(sut.count, 1, "Count should be same as number of photos added")
+        sut.removeAll()
+        let noItem = sut.photoAtIndex(0)
+        XCTAssertNil(noItem, "There should be no items after removeAll")
+    }
 }
